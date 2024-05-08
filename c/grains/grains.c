e8fbf1680394f294a44eaee501932dc562fc2363
Lukas Eder <ederlukas@bluewin.ch>
2024-05-08 23:18:18 +0200

    implemented grains exercise in c

---
+++ c/grains/grains.c
@@ -0,0 +1,16 @@
+#include "grains.h"
+
+uint64_t square(uint8_t index) {
+    if (index < 1 || index > 64) {
+        return 0;
+    }
+    if (index == 1) {
+        return 1;
+    }
+
+    return (uint64_t) pow(2, --index);
+}
+
+uint64_t total() {
+    return (uint64_t) pow(2, 64);
+}
---
+++ c/grains/grains.h
@@ -0,0 +1,11 @@
+#ifndef GRAINS_H
+#define GRAINS_H
+
+#include <stdint.h>
+#include <math.h>
+#include <stdio.h>
+
+uint64_t square(uint8_t index);
+uint64_t total(void);
+
+#endif
